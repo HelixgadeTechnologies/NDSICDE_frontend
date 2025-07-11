d685a372823b9fe315230485a59e9adcbb5a2d3c
estherchinda <davidakachi9@gmail.com>
2025-07-11 23:25:08 +0100

    Remove .next build directory from version control

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"koAxo38x/hrBokBFnCkKOrJ743D3OIpoIPT4XZnyWYE=\"\n}"
--- .next/static/chunks/app_login_page_tsx_c54eba18._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_f0dd77b7._.js",
-  "static/chunks/_28c9fbaa._.js"
-],
-    source: "dynamic"
-});
