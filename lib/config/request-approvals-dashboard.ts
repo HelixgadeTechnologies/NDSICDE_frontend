f10af8bbc429f80170237055405039489b7b182f
estherchinda <davidakachi9@gmail.com>
2025-10-24 21:56:36 +0100

    added request approval stat cards

---
+++ lib/config/request-approvals-dashboard.ts
@@ -0,0 +1,50 @@
+export const activity_financial_request = [
+    {
+      title: "Total Request",
+      value: "0",
+      icon: "mdi:chart-box-outline",
+    },
+    {
+      title: "Approved Requests",
+      value: "0",
+      icon: "mdi:check-circle-outline",
+    },
+    {
+      title: "Rejected Requests",
+      value: "0",
+      icon: "mdi:close-circle-outline",
+    },
+    {
+      title: "Pending Requests",
+      value: "0",
+      icon: "mdi:clock-outline",
+    },
+  ];
+
+  export const activity_financial_retirement = [
+    {
+      title: "Total Request",
+      value: "0",
+      icon: "mdi:chart-box-outline",
+    },
+    {
+      title: "Total Retired",
+      value: "0",
+      icon: "mdi:check-circle-outline",
+    },
+    {
+      title: "Total Rejected",
+      value: "0",
+      icon: "mdi:close-circle-outline",
+    },
+    {
+      title: "Total Under Review",
+      value: "0",
+      icon: "mdi:clock-outline",
+    },
+     {
+      title: "Total Pending for Review",
+      value: "0",
+      icon: "mdi:clock-outline",
+    },
+  ];
