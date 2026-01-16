ee8b70a8479db08acf05a002a698b155c2c096ab
estherchinda <davidakachi9@gmail.com>
2026-01-16 14:13:16 +0100

    added create and del on request

--- app/(app)/projects/[id]/project-management/activity/page.tsx
+++ app/(app)/projects/[id]/project-management/activity/page.tsx
@@ -73,24 +73,25 @@ export default function ProjectActivity() {
   const deleteActivity = async (activityId: string) => {
     setIsDeleting(true);
     try {
-       const res = await axios.delete(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projectManagement/activity/${activityId}`, 
+      const res = await axios.delete(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/projectManagement/activity/${activityId}`,
         {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
-          }
+          },
         }
       );
       toast.success("Project activity deleted successfully!");
-      setRemoveActivity(false)
+      setRemoveActivity(false);
       fetchActivities();
     } catch (error) {
       console.error(`Error deleting activity: ${error}`);
-      toast.error("An error occurred. Please try again later.")
+      toast.error("An error occurred. Please try again later.");
     } finally {
       setIsDeleting(false);
     }
-  }
+  };
 
   return (
     <div className="relative mt-12">
@@ -103,82 +104,88 @@ export default function ProjectActivity() {
       </div>
 
       <CardComponent>
-        <Table
-          tableHead={head}
-          tableData={data}
-          checkbox
-          idKey={"activityId"}
-          renderRow={(row) => (
-            <>
-              <td className="px-6">{row.activityStatement}</td>
-              <td className="px-6">{row.subActivity}</td>
-              <td className="px-6">{row.activityTotalBudget}</td>
-              <td className="px-6">{formatDate(row.startDate, "short")}</td>
-              <td className="px-6">{formatDate(row.endDate, "short")}</td>
-              <td className="px-6">{row.responsiblePerson}</td>
-              <td className="px-6 relative">
-                <Icon
-                  icon={"uiw:more"}
-                  width={22}
-                  height={22}
-                  className="cursor-pointer"
-                  color="#909CAD"
-                  onClick={() =>
-                    setActiveRowId((prev) =>
-                      prev === row.activityId ? null : row.activityId
-                    )
-                  }
-                />
+        {isLoading ? (
+          <div className="dots ny-20 mx-auto">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        ) : (
+          <Table
+            tableHead={head}
+            tableData={data}
+            checkbox
+            idKey={"activityId"}
+            renderRow={(row) => (
+              <>
+                <td className="px-6">{row.activityStatement}</td>
+                <td className="px-6">{row.subActivity}</td>
+                <td className="px-6">{row.activityTotalBudget}</td>
+                <td className="px-6">{formatDate(row.startDate, "short")}</td>
+                <td className="px-6">{formatDate(row.endDate, "short")}</td>
+                <td className="px-6">{row.responsiblePerson}</td>
+                <td className="px-6 relative">
+                  <Icon
+                    icon={"uiw:more"}
+                    width={22}
+                    height={22}
+                    className="cursor-pointer"
+                    color="#909CAD"
+                    onClick={() =>
+                      setActiveRowId((prev) =>
+                        prev === row.activityId ? null : row.activityId
+                      )
+                    }
+                  />
 
-                {activeRowId === row.activityId && (
-                  <AnimatePresence>
-                    <motion.div
-                      initial={{ y: -10, opacity: 0 }}
-                      animate={{ y: 0, opacity: 1 }}
-                      exit={{ y: -10, opacity: 0 }}
-                      transition={{ duration: 0.2, ease: "easeOut" }}
-                      className="absolute top-full mt-2 right-0 bg-white z-30 rounded-md border border-[#E5E5E5] shadow-md w-50">
-                      <ul className="text-sm">
-                        <li
-                          onClick={() =>
-                            handleEditActivity(row, setActiveRowId)
-                          }
-                          className="cursor-pointer hover:text-blue-600 flex gap-2 p-3 items-center">
-                          <Icon
-                            icon={"ph:pencil-simple-line"}
-                            height={20}
-                            width={20}
-                          />
-                          Edit
-                        </li>
-                        <li
-                          onClick={() =>
-                            handleRemoveActivity(row, setActiveRowId)
-                          }
-                          className="cursor-pointer hover:text-(--primary-light) border-y border-gray-300 flex gap-2 p-3 items-center">
-                          <Icon
-                            icon={"pixelarticons:trash"}
-                            height={20}
-                            width={20}
-                          />
-                          Remove
-                        </li>
-                        <Link
-                          href={
-                           `/projects/${projectId}/project-management/activity/report-actual-value`
-                          }
-                          className="cursor-pointer hover:text-blue-600 flex gap-2 p-3 items-center">
-                          <Icon icon={"si:add-fill"} height={20} width={20} />
-                          Report Actual Value
-                        </Link>
-                      </ul>
-                    </motion.div>
-                  </AnimatePresence>
-                )}
-              </td>
-            </>
-          )}
-        />
+                  {activeRowId === row.activityId && (
+                    <AnimatePresence>
+                      <motion.div
+                        initial={{ y: -10, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{ y: -10, opacity: 0 }}
+                        transition={{ duration: 0.2, ease: "easeOut" }}
+                        className="absolute top-full mt-2 right-0 bg-white z-30 rounded-md border border-[#E5E5E5] shadow-md w-50">
+                        <ul className="text-sm">
+                          <li
+                            onClick={() =>
+                              handleEditActivity(row, setActiveRowId)
+                            }
+                            className="cursor-pointer hover:text-blue-600 flex gap-2 p-3 items-center">
+                            <Icon
+                              icon={"ph:pencil-simple-line"}
+                              height={20}
+                              width={20}
+                            />
+                            Edit
+                          </li>
+                          <li
+                            onClick={() =>
+                              handleRemoveActivity(row, setActiveRowId)
+                            }
+                            className="cursor-pointer hover:text-(--primary-light) border-y border-gray-300 flex gap-2 p-3 items-center">
+                            <Icon
+                              icon={"pixelarticons:trash"}
+                              height={20}
+                              width={20}
+                            />
+                            Remove
+                          </li>
+                          <Link
+                            href={`/projects/${projectId}/project-management/activity/report-actual-value`}
+                            className="cursor-pointer hover:text-blue-600 flex gap-2 p-3 items-center">
+                            <Icon icon={"si:add-fill"} height={20} width={20} />
+                            Report Actual Value
+                          </Link>
+                        </ul>
+                      </motion.div>
+                    </AnimatePresence>
+                  )}
+                </td>
+              </>
+            )}
+          />
+        )}
       </CardComponent>
 
       {selectedActivity && (
@@ -188,7 +195,6 @@ export default function ProjectActivity() {
           mode="edit"
           initialData={selectedActivity}
           onSuccess={fetchActivities}
-          
         />
       )}
       {selectedActivity && (
