cbd46efc8d612236d2a53c05a998b7c61e81fb39
estherchinda <davidakachi9@gmail.com>
2026-01-28 15:16:38 +0100

    added api calls to management and staff

--- lib/config/general-config.ts
+++ lib/config/general-config.ts
@@ -1,8 +1,18 @@
 const currentYear = new Date().getFullYear();
+
 export const year_options = Array.from(
-  { length: currentYear - 1990 + 1 },
+  { length: currentYear - 2015 + 1 },
   (_, i) => {
-    const year = 1990 + i;
+    const year = 2015 + i;
     return { label: String(year), value: String(year) };
   }
 );
+
+// Format currency
+  export const formatCurrency = (amount: number) => {
+    return new Intl.NumberFormat('en-NG', {
+      style: 'currency',
+      currency: 'NGN',
+      minimumFractionDigits: 0,
+    }).format(amount);
+  };
