0b3977fd8ab58497424742981e10ba34b508441e
estherchinda <davidakachi9@gmail.com>
2026-02-13 16:31:17 +0100

    changed loader on financial reporting

--- app/(app)/financial-reporting/page.tsx
+++ app/(app)/financial-reporting/page.tsx
@@ -80,8 +80,10 @@ export default function FinancialReporting() {
         <DashboardStat data={dashboardData} />
       </div>
       {loading ? (
-        <div className="flex items-center justify-center h-64">
-          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+        <div className="dots my-20 mx-auto">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       ) : (
         <>
